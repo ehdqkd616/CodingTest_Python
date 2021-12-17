3b34739fb342b03e1c70e7e22f9f12bfe1e23966
kyw0216 <ehdqkd616@cau.ac.kr>
2021-12-17 15:29:58 +0900

    코드업 파이선 6001 - 6024

---
+++ CodeUp/6013.py
@@ -0,0 +1,11 @@
+# 6013 : [기초-입출력] 문자 2개 입력받아 순서 바꿔 출력하기1(py)
+
+# 본 문제는 python 의 빠른 기초 학습을 위해 설계된 문제로서 python 코드 제출을 기준으로 설명되어 있습니다.
+# ------
+
+# 줄을 바꿔 문자(character) 2개를 입력받고, 순서를 바꿔 한 줄씩 출력해보자.
+
+variable1 = input()
+variable2 = input()
+print(variable2)
+print(variable1)
