f93bc4c18780a6d8b2b7e3af7c5a3ae19c046062
ehdqkd616 <64412357+ehdqkd616@users.noreply.github.com>
2022-04-11 16:41:16 +0900

    dd

---
+++ Baekjoon/Silver III/[5397] 키로거 copy 2.py
@@ -0,0 +1,19 @@
+﻿import sys
+
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    sL, sR = [], []
+    for i in input().strip():
+        if i == '<':
+            if sL:
+                sR.append(sL.pop())
+        elif i == '>':
+            if sR:
+                sL.append(sR.pop())
+        elif i == '-':
+            if sL:
+                sL.pop()
+        else:
+            sL.append(i)
+    print(''.join(sL)+''.join(sR)[::-1])
